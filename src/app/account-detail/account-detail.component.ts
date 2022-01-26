805479165c986d362f2e4cbec8ba1d04e2600e39
Mark Fasel <mark.fasel@scorpion.co>
2022-01-26 10:48:19 -0500

    Using Observable Subscription for route params

--- src/app/account-detail/account-detail.component.ts
+++ src/app/account-detail/account-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-account-detail',
@@ -16,7 +16,11 @@ export class AccountDetailComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.id = Number(this.route.snapshot.paramMap.get('id'));
+    // this.id = Number(this.route.snapshot.paramMap.get('id'));
+    // Can also get route param subscribing to observable
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.id = Number(params.get('id'));
+    });
   }
 
 }
